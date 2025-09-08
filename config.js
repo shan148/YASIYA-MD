4110b1c80f1584f78b60710ee86c3a46b6948be2
shan148 <shanb5845@gmail.com>
2025-09-08 19:55:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~jgJAQbgR#kGeI8Kuuf7ZnzfzEVOfpJXKr5isLHKL1OxcHDFU35dE'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~25oW1KYD#QSt7bhzqzxQMnF7YQlozvS3-GhWmYi8ngK24-n2FGrU'
 };
